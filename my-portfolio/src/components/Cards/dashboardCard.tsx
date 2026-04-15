9c7cc0d1b3484ca9725f9743c250dfe7ce8fd121
aishwinder <aishwinder.sandhu@gmail.com>
2026-04-15 14:56:16 +0530

    Dashboard cards implementation in progress

---
+++ my-portfolio/src/components/Cards/dashboardCard.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import '../../styles/dashboard.css';
+
+export const VerticalStrips =() =>{
+  return (
+    <div className="dashboard-card-wrapper">
+      <div className="dashboard-card">
+          {/* Vertical color rendering from props */}
+      </div>
+    </div>
+  )
+}
--- my-portfolio/src/components/colorAnalysis.tsx
+++ my-portfolio/src/components/colorAnalysis.tsx
@@ -2,6 +2,7 @@ import { RootState } from "../redux/store";
 import { useSelector } from "react-redux";
 import '../styles/colorAnalysis.css';
 import { AnalysisCard } from "./AnalysisCard";
+import { VerticalStrips } from "./Cards/dashboardCard";
 
 export const ColorAnalysis = () => {
   const { showBoard } = useSelector((state: RootState) => state.utilsReducer.showBoard);
@@ -12,7 +13,8 @@ export const ColorAnalysis = () => {
       <AnalysisCard value={94} title={'Color match'}></AnalysisCard>
       <AnalysisCard value={12} title={'Wear Colors'}></AnalysisCard>
       <AnalysisCard value={6} title={'Avoid Colors'}></AnalysisCard>
-
     </div>
+     {/*TO-DO: Make it a fun way to show random cards */}
+      <VerticalStrips></VerticalStrips>
   </div>)
 }
